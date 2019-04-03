c4c5736672062c2bc6d29615c440a7023d6a9e5b
GCMaia <gabriel.cardosomaia@gmail.com>
2019-04-03 15:11:44 -0400

    Update test.js

--- test.js
+++ test.js
@@ -3,8 +3,8 @@ $(document).ready(function(){
 	var typed = new Typed('#typed',{
 		
 		strings: ['FINALLY HOLY MY', "Testing another"],
-		typeSpeed: 100,//specifies the typing speed of the text
-		backSpeed: 100, //specifies speed of deletion of the text
+		typeSpeed: 10,//specifies the typing speed of the text
+		backSpeed: 10, //specifies speed of deletion of the text
 		loop: true, //repetition
 		showCursor: false
 	});
