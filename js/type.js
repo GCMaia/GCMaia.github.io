e1c4354c0ce544ba2650ef3ae6c8a4ea994edce6
GCMaia <gabriel.cardosomaia@gmail.com>
2019-04-03 15:47:43 -0400

    Add files via upload

---
+++ js/type.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+
+	var typed = new Typed('#typed',{
+		
+		strings: ['Seeing something new @ MNP ', 'Playing something', 'Getting frustrated while coding', 'Talking to some friends', 'Learning something new', 
+		'Trying to help someone', 'Trying to Parkour', 'Understanding some new microsoft tool', 'looking for some spiderman content', 'Giving some programming tutoring', 
+		'Trying to cook something different', 'Forgetting about something important', 'Finishing my course @ Algonquin College' ],
+		typeSpeed: 80,
+		backSpeed: 50, 
+		loop: true, 
+		showCursor: true,
+		shuffle: true,
+		backDelay: 1000
+	});
+
+});
