f6b87cef742a8d1c65fe417dbdb69b1cf7ee895a
GCMaia <gabriel.cardosomaia@gmail.com>
2019-04-03 15:10:20 -0400

    Update test.js

--- test.js
+++ test.js
@@ -5,7 +5,8 @@ $(document).ready(function(){
 		strings: ['FINALLY HOLY MY', "Testing another"],
 		typeSpeed: 100,//specifies the typing speed of the text
 		backSpeed: 100, //specifies speed of deletion of the text
-		loop: true //repetition
+		loop: true, //repetition
+		showCursor: false
 	});
 
 });
