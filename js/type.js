fb4c00aafdf200f35da3aa684de5534c602c32a3
GCMaia <gabriel.cardosomaia@gmail.com>
2019-04-04 08:42:00 -0400

    Update type.js

--- js/type.js
+++ js/type.js
@@ -2,15 +2,14 @@ $(document).ready(function(){
 
 	var typed = new Typed('#typed',{
 		
-		strings: ['Seeing something new @ MNP ', 'Playing something', 'Getting frustrated while coding', 'Talking to some friends', 'Learning something new', 
+		strings: ['Seeing something new @ MNP ', 'Playing something', 'Getting frustrated while coding','Getting amazed while coding' ,'Talking to some friends', 'Learning something new', 
 		'Trying to help someone', 'Trying to Parkour', 'Understanding some new microsoft tool', 'looking for some spiderman content', 'Giving some tutoring', 
 		'Trying to cook something different', 'Forgetting about something important', 'Finishing my course @ Algonquin College', 'Watching some Marvel Movie',
-			'Writting hello world in a new language :)', 'getting richer'],
+		'Writting hello world in a new language :)', 'getting richer'],
 		typeSpeed: 80,
 		backSpeed: 50, 
 		loop: true, 
 		showCursor: true,
-		shuffle: true,
 		backDelay: 1000
 	});
 
