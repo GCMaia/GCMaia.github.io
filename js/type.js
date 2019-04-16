1eb8a646be2ffe15a793fed54b3fc9cf038864dc
GCMaia <gabriel.cardosomaia@gmail.com>
2019-04-16 14:24:01 -0400

    Add files via upload

--- js/type.js
+++ js/type.js
@@ -2,7 +2,8 @@ $(document).ready(function(){
 
 	var typed = new Typed('#typed',{
 		
-		strings: ['Seeing something new @ MNP ', '01000000 00100000 01001101 01001110 01010000' ,'Playing something', 'Getting frustrated while coding','Getting amazed while coding' ,'Talking to some friends', 'Learning something new', 
+		strings: ['Seeing something new @ MNP ', '00110010 00110001 00101111 00110000 00110001 00101111 00110001 00111001 00111001 00110111' ,'Playing something', 
+		'Getting frustrated while coding','Getting amazed while coding' ,'Talking to some friends', 'Learning something new', 
 		'Trying to help someone', 'Trying to Parkour', 'Understanding some new microsoft tool', 'looking for some spiderman content', 'Giving some tutoring', 
 		'Trying to cook something different', 'Forgetting about something important', 'Finishing my course @ Algonquin College', 'Watching some Marvel Movie',
 		'Writting hello world in a new language :)', 'getting richer'],
