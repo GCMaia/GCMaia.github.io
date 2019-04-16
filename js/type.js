109493c39ab5be7058a8463da184bc3da595d3f2
GCMaia <gabriel.cardosomaia@gmail.com>
2019-04-16 15:45:44 -0400

    Add files via upload

--- js/type.js
+++ js/type.js
@@ -2,17 +2,60 @@ $(document).ready(function(){
 
 	var typed = new Typed('#typed',{
 		
-		strings: ['Seeing something new @ MNP ', '00110010 00110001 00101111 00110000 00110001 00101111 00110001 00111001 00111001 00110111' ,'Playing something', 
+		strings: ['Seeing something new @ MNP ' ,'Playing something', 'Excited for the new Avengers Movie',
 		'Getting frustrated while coding','Getting amazed while coding' ,'Talking to some friends', 'Learning something new', 
-		'Trying to help someone', 'Trying to Parkour', 'Understanding some new microsoft tool', 'looking for some spiderman content', 'Giving some tutoring', 
+		'Trying to help someone', 'Understanding some new microsoft tool', 'looking for some spiderman content', 'Giving some tutoring', 
 		'Trying to cook something different', 'Forgetting about something important', 'Finishing my course @ Algonquin College', 'Watching some Marvel Movie',
-		'Writting hello world in a new language :)', 'getting richer'],
-		typeSpeed: 50,
-		backSpeed: 30, 
+		'Writting hello world in a new language :)'],
+		typeSpeed: 25,
+		backSpeed: 15, 
 		loop: true, 
 		showCursor: true,
-		backDelay: 1500
+		backDelay: 1700,
 	});
 
+	document.getElementById("default").click();
+
 });
+
+
+function openInfo(evt, cityName) {
+	var i, tabcontent, tablinks;
+	tabcontent = document.getElementsByClassName("tabcontent");
+	for (i = 0; i < tabcontent.length; i++) {
+		tabcontent[i].style.display = "none";
+	}
+	tablinks = document.getElementsByClassName("tablinks");
+	for (i = 0; i < tablinks.length; i++) {
+		tablinks[i].className = tablinks[i].className.replace(" active", "");
+	}
+	document.getElementById(cityName).style.display = "block";
+	evt.currentTarget.className += " active";
+
+	if (cityName == 'Languages' || cityName == 'Skills') {
+
+		var elem = document.getElementsByClassName("testingBar");
+		for (i = 0; i < elem.length; i++) {
+			var width = 1;
+			var id = setInterval(frame, 40);
+			function frame() {
+				if (width >= 100) {
+					clearInterval(id);
+				} else {
+					width++;
+					elem[0].style.width = width + '%';
+					elem[1].style.width = width + '%';
+					elem[2].style.width = width + '%';
+					elem[3].style.width = width + '%';
+					elem[4].style.width = width + '%';
+					elem[5].style.width = width + '%';
+					elem[6].style.width = width + '%';
+					elem[7].style.width = width + '%';
+				}
+			}
+		}
+	}
+}
+
+// Get the element with id="defaultOpen" and click on it
 
