1a6be2ed4674459a0168fbb2b20e43a4cb64c86a
Gabriel Maia <gabriel.cardosomaia@gmail.com>
2023-05-01 00:15:11 -0400

    trying to make bar changes

--- js/makeItAlive.js
+++ js/makeItAlive.js
@@ -32,14 +32,15 @@ function openInfo(evt, tabName) {
 	evt.currentTarget.className += " active";
 
     if (tabName == 'Languages') {
-        var languageItems = ["intermediate", ""];
-        var elem = document.getElementsByClassName("testingBar");
+		var languageItems = ["intermediate", "advanced"];
+        var elem = document.getElementsByClassName("languageBar");
         for (i = 0; i < languageItems.length; i++) {
             
 
 
 
-        }
+		}
+
     } else if (tabName == 'Skills') {
         var skillItems = [];
         var elem = document.getElementsByClassName("testingBar");
